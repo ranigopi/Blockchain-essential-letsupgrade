f1f36464b06d52dd2932beba7d51889792bca3bd
ranigopi <68233604+ranigopi@users.noreply.github.com>
2020-07-26 13:51:29 +0530

    Create Day 2 Assignment.sol

---
+++ Day 2 Assignment.sol
@@ -0,0 +1,19 @@
+progra ^0.4.17
+contract land{
+  string public owner;
+  string public value;
+  string public location;
+function land(string.newowner,string.newvalue,string.newlocation)public{
+owner = newowner;
+value = newvalue;
+location = newlocation;
+}
+function setdetails(string.newowner,string.newvalue,string.newlocation){
+owner = newowner;
+value = newvalue;
+location = newlocation;
+}
+function getdetails() public view return(string,string,string){
+return (owner,value, location);
+}
+}
